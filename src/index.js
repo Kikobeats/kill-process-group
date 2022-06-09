aa82b40a90103a6a8b76d4590abf210c97c68cfe
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-06-10 00:04:25 +0200

    fix: ensure API is homogeneous

--- src/index.js
+++ src/index.js
@@ -5,24 +5,24 @@ const exec = promisify(require('child_process').exec)
 
 const isWin = process.platform === 'win32'
 
-const win = async proc => {
+const win = async subprocess => {
   try {
-    await exec(`taskkill /pid ${proc.pid} /T /F`)
+    await exec(`taskkill /pid ${subprocess.pid} /T /F`)
   } catch (_) {
     // taskkill can fail to kill the process e.g. due to missing permissions.
     // Let's kill the process via Node API. This delays killing of all child
     // processes of `this.proc` until the main Node.js process dies.
-    proc.kill()
+    subprocess.kill()
   }
 }
 
-const unix = (subprocess, { signal = 'SIGKILL' } = {}) => {
+const unix = async (subprocess, { signal = 'SIGKILL' } = {}) => {
   // on linux the process group can be killed with the group id prefixed with
   // a minus sign. The process group id is the group leader's pid.
   const processGroupId = -subprocess.pid
   try {
     process.kill(processGroupId, signal)
-  } catch (error) {
+  } catch (_) {
     // Killing the process group can fail due e.g. to missing permissions.
     // Let's kill the process via Node API. This delays killing of all child
     // processes of `this.proc` until the main Node.js process dies.
